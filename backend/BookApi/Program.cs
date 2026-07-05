114069af3edf15bb0e45df6e3c931f7375b88e3d
Jesna-Sunny <jesnasunnydr2001@gmail.com>
2026-07-05 21:57:38 +0200

    Add Book CRUD endpoints and fix Swagger setup

--- backend/BookApi/Program.cs
+++ backend/BookApi/Program.cs
@@ -1,19 +1,22 @@
 using Microsoft.EntityFrameworkCore;
 using BookApi.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite("Data Source=bookapi.db"));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
