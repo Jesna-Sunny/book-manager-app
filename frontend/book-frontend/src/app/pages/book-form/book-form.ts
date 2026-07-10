d3c3fcbc0cc488e5c9e1421bdd0e38b9c6d36569
Jesna-Sunny <jesnasunnydr2001@gmail.com>
2026-07-10 23:26:37 +0200

    Set up Angular routing, navbar, and home page with book list

--- frontend/book-frontend/src/app/pages/book-form/book-form.ts
+++ frontend/book-frontend/src/app/pages/book-form/book-form.ts
@@ -1,11 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Book } from '../../models/book';
+import { BookService } from '../../services/book';
 
 @Component({
   selector: 'app-book-form',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './book-form.html',
   styleUrl: './book-form.scss',
 })
-export class BookForm {
+export class BookForm implements OnInit {
+  book: Partial<Book> = { title: '', author: '', publicationDate: '' };
+  isEditMode = false;
+  bookId: number | null = null;
 
+  constructor(
+    private bookService: BookService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    const idParam = this.route.snapshot.paramMap.get('id');
+    if (idParam) {
+      this.isEditMode = true;
+      this.bookId = +idParam;
+      this.bookService.getBook(this.bookId).subscribe({
+        next: (data) => (this.book = data),
+        error: (err) => console.error('Failed to load book', err),
+      });
+    }
+  }
+
+  onSubmit(): void {
+    if (this.isEditMode && this.bookId !== null) {
+      this.bookService.updateBook(this.bookId, this.book as Book).subscribe({
+        next: () => this.router.navigate(['/']),
+        error: (err) => console.error('Failed to update book', err),
+      });
+    } else {
+      this.bookService.addBook(this.book).subscribe({
+        next: () => this.router.navigate(['/']),
+        error: (err) => console.error('Failed to add book', err),
+      });
+    }
+  }
 }
--- frontend/book-frontend/src/app/pages/home/home.ts
+++ frontend/book-frontend/src/app/pages/home/home.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { Book } from '../../models/book';
+import { BookService } from '../../services/book';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [RouterLink, CommonModule],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
-export class Home {
+export class Home implements OnInit {
+  books: Book[] = [];
 
+  constructor(private bookService: BookService) {}
+
+  ngOnInit(): void {
+    this.loadBooks();
+  }
+
+  loadBooks(): void {
+    this.bookService.getBooks().subscribe({
+      next: (data) => (this.books = data),
+      error: (err) => console.error('Failed to load books', err),
+    });
+  }
+
+  deleteBook(id: number): void {
+    if (confirm('Delete this book?')) {
+      this.bookService.deleteBook(id).subscribe({
+        next: () => this.loadBooks(),
+        error: (err) => console.error('Failed to delete book', err),
+      });
+    }
+  }
 }
